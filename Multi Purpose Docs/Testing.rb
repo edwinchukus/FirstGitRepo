d9fbff85af4d297c4d9ecf4c9d83c1f89a0d7261
edwinchukus <edwin.nwofor@yahoo.com>
2014-09-26 16:16:46 +0100

    Update

---
+++ Multi Purpose Docs/Testing.rb
@@ -0,0 +1,39 @@
+
+ @x = (gets.chomp).to_i  
+
+ class Edwin
+ def talk 
+ if @x > 2
+puts y = @x **2
+
+end 
+end 
+end 
+
+=begin
+class Edwin
+
+  
+def talk
+	if @x > 0
+#begin
+   #y = (@x)**2
+   #puts y
+   puts "Hello I am just checking something"
+#end
+	else
+	puts "The square of the number is 0"
+	end 
+	
+  end
+
+
+=end
+
+
+  
+@dog = Edwin.new
+@dog.talk
+
+
+
